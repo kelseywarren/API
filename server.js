8ebc5031ac35b0532dc1d91bc020bf8f261f6387
kelseywarren <warrenkelsey@outlook.com>
2024-06-08 16:44:59 -0500

    Add rbc endpoint and response to server

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const wbc = require('./data/wbc-data.js');
 const rbc = require('./data/rbc-data.js');
 
 app.get('/wbc', (req, res) => {
-    res.send(wbc)
-})
+    res.send(wbc);
+});
+
+app.get('/rbc', (req, res) => {
+    res.send(rbc);
+});
 
 
 // PORT
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
-})
+});
