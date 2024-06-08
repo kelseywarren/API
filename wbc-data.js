acbc5b70b574318508843ad3c265541af8e57ee7
kelseywarren <warrenkelsey@outlook.com>
2024-06-08 11:39:31 -0500

    Add base wbc data structure

---
+++ wbc-data.js
@@ -0,0 +1,47 @@
+const wbc = {
+    data: [
+        {   
+            id: 1,
+            name: 'name',
+            shortName: 'name',
+            description: 'text',
+            image: 'url'
+
+        },
+        {   
+            id: 2,
+            name: 'name',
+            shortName: 'name',
+            description: 'text',
+            image: 'url'
+
+        },
+        {   
+            id: 3,
+            name: 'name',
+            shortName: 'name',
+            description: 'text',
+            image: 'url'
+
+        },
+        {   
+            id: 4,
+            name: 'name',
+            shortName: 'name',
+            description: 'text',
+            image: 'url'
+
+        },
+        {   
+            id: 5,
+            name: 'name',
+            shortName: 'name',
+            description: 'text',
+            image: 'url'
+
+        },
+    ]
+}
+
+module.exports = wbc; 
+
